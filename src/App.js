48db536b5ac39ce625568ba0209f1ee0e58478ca
davidesiracusa <davide.siracusa@bbzsogr.ch>
2024-05-16 20:24:05 +0200

    Background changed with a cloud

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
+import Cloud1 from './Background/cloud-svgrepo-com (2).svg'; // Importiere deine SVG-Dateien hier
+import Cloud2 from './Background/cloud-svgrepo-com (2).svg';
+import Cloud3 from './Background/cloud-svgrepo-com (2).svg';
+import Cloud4 from './Background/cloud-svgrepo-com (2).svg';
 
 function App() {
-  const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  };
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      <div className="background-with-clouds">
+        <img src={Cloud1} alt="Cloud" className="clouds" />
+        <img src={Cloud2} alt="Cloud" className="clouds" />
+        <img src={Cloud3} alt="Cloud" className="clouds" />
+        <img src={Cloud4} alt="Cloud" className="clouds" />
+      </div>
+      <div className="content">
+        <Search />
+        <CurrentWeather />
+      </div>
     </div>
   );
 }
