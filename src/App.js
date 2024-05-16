95f3ad6921aeb13751bd3c0e57171645faeb0efc
e06a <erdi.ajeti@bbzsogr.ch>
2024-05-16 11:00:41 +0200

    dummy printing hello for forecast implementation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
+import React, {useState} from 'react';
+import {WEATHER_API_KEY, WEATHER_API_URL} from './api';
 import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
 import Forecast from './components/weather-forecast/forecast';
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const [forecast, setForecast] = useState(null);
 
-  const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split('');
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    const handleOnSearchChange = (searchData) => {
+        const [lat, lon] = searchData.value.split('');
+        const currentWeatherFetch = fetch(
+            `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        );
+        const forecastFetch = fetch(
+            `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        );
+
+        Promise.all([currentWeatherFetch, forecastFetch])
+            .then(async (response) => {
+                const weatherResponse = await response[0].json();
+                const forecastResponse = await response[1].json();
+
+                setCurrentWeather({city: searchData.label, ...weatherResponse});
+                setForecast({city: searchData.label, ...forecastResponse});
+            })
+            .catch((err) => console.log(err));
+    };
+
+    console.log(currentWeather);
+    console.log(forecast);
+    return (
+        <div className="container">
+            <Search onSearchChange={handleOnSearchChange}/>
+            {currentWeather && <CurrentWeather data={currentWeather}/>}
+            {forecast && <Forecast data={forecast}/>}
+        </div>
     );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forecastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forecastResponse });
-      })
-      .catch((err) => console.log(err));
-  };
-
-  console.log(currentWeather);
-  console.log(forecast);
-  return (
-    <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      <Forecast />
-    </div>
-  );
 }
 
 export default App;
--- src/components/weather-forecast/forecast.js
+++ src/components/weather-forecast/forecast.js
@@ -1,21 +1,30 @@
 import {
-  Accordion,
-  AccordionItem,
-  AccordionItemHeading,
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel
 } from 'react-accessible-accordion';
 
-const Forecast = ({ data }) => {
-  return (
-    <>
-      <label className="">Daily </label>
-      <Accordion allowZeroExpanded>
-        {data.list.splice(0, 7).map((item, idx) => (
-          <AccordionItemHeading></AccordionItemHeading>
-        ))}
-        <AccordionItem></AccordionItem>
-      </Accordion>
-    </>
-  );
+const Forecast = ({data}) => {
+    return (
+        <>
+            <label className="title">Daily </label>
+            <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                hello
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
+                ))}
+                <AccordionItem></AccordionItem>
+            </Accordion>
+        </>
+    );
 };
 
 export default Forecast;
